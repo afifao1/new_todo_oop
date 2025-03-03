c895dba00147af8eaff210b4b4f8630cc5dc982d
afifao1 <ruhsonaholmirzaeva85@gmail.com>
2025-03-03 11:37:33 +0500

    Added .env

--- helpers.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-function dump($arg){
-  echo "<pre>";
-  var_dump($arg);
-  echo "</pre>";
-}
--- public/view.php
+++ public/view.php
@@ -1,48 +1,48 @@
 <?php
-require_once 'controllers/Web2.php';
+require_once '../controllers/Web2.php';
 
-$todo = new Web2();
+$todo = new Web();
 
 if(isset($_POST['task'])) {
-  $todo->addTask($_POST['task']);
+  echo $todo->addTask($_POST['task']); // <-- Echo qo‘shildi
 }
-
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="./public/style.css">
-        <title>Document</title>
+        <title>ToDo App</title>
     </head>
     <body>
         <div class="wrapper">
             <div class="container">
-            <h1>ToDo App</h1>
-            <form action="./view.php" method="post">
-                <input type="text" name="task" id="" placeholder="Task qo'shing">
-                <button class="" type="submit">Add task</button>
-            </form>
-            <ul class="list">
-            <?php
-                $tasks = $todo->getTasks();
-                
-                if(count($tasks) > 0) {
-                    foreach($tasks as $task) {
-                        $status = $task['status'] ? '✅' :'🟢';
-                        echo "
-                        <li>
-                        <a class='link' href='?id={$task['id']}'>$status {$task['task']}</a>
-                        <a href='?delete={$task['id']}'>Delete</a>
-                        </li>";
-                    }
-                } else {
-                    echo "<h1>Please add a task.</h1>";
-                }
-            ?>
+                <h1>ToDo App</h1>
+                <form action="" method="post"> <!-- action="" bo‘lishi kerak -->
+                    <input type="text" name="task" id="" placeholder="Task qo'shing">
+                    <button class="" type="submit">Add task</button>
+                </form>
+                <ul class="list">
+                    <?php
+                        $tasks = $todo->getTasks();
+                        
+                        if(count($tasks) > 0) {
+                            foreach($tasks as $task) {
+                                $status = $task['status'] ? '✅' :'🟢';
+                                echo "
+                                <li>
+                                    <a class='link' href='?id={$task['id']}'>$status {$task['task']}</a>
+                                    <a href='?delete={$task['id']}'>Delete</a>
+                                </li>";
+                            }
+                        } else {
+                            echo "<h1>Please add a task.</h1>";
+                        }
+                    ?>
+                </ul>
             </div>
-        </ul>
-    </div>
-</body>
+        </div>
+    </body>
 </html>
